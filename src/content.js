61d122e9f6b1144af02c06eda1428ac6379e9f4d
David Mihal <eastsideski@gmail.com>
2015-09-21 16:56:04 -0400

    Function to add a slider UI element

--- src/content.js
+++ src/content.js
@@ -7,3 +7,9 @@ require(['services/chromecast'], function(chromecast_promise){
     };
   });
 });
+var attachSlider = function(){
+  var div = document.createElement('div');
+  div.style.cssText = "position: absolute; width: 100px; top: -50px; color: white; border: 1px solid #333; padding: 0 7px;";
+  div.innerHTML = '<div class="progress"><div class="seekbar"></div><div class="episode-progress" style="width: 61%;"></div></div>';
+  document.querySelector('li.control.mute').appendChild(div);
+};
