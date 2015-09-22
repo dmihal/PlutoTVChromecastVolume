835efb752aaa007004a4fb1725388fdd70513ada
David Mihal <eastsideski@gmail.com>
2015-09-22 12:04:13 -0400

    Wait for main module to load before initializing

--- src/content.js
+++ src/content.js
@@ -1,9 +1,11 @@
-require(['services/chromecast'], function(chromecast_promise){
-  chromecast_promise.then(function(chromecast){
-    window.setVolume = function(level){
-      if (chromecast.map.isReceiverAvailable){
-        chromecast.map.session.setReceiverVolumeLevel(level, function(){console.log('success')}, function(){console.log('fail')});
-      }
-    };
+require(['main'], function(){
+  require(['services/chromecast'], function(chromecast_promise){
+    chromecast_promise.then(function(chromecast){
+      window.setVolume = function(level){
+        if (chromecast.map.isReceiverAvailable){
+          chromecast.map.session.setReceiverVolumeLevel(level, function(){console.log('success')}, function(){console.log('fail')});
+        }
+      };
+    });
   });
 });
