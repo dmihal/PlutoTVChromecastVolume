975e7e88847ccc8e72cbfd6ab5d72919d9a5ddd8
David Mihal <eastsideski@gmail.com>
2015-09-22 17:32:56 -0400

    Update slider style

--- src/content.js
+++ src/content.js
@@ -11,7 +11,7 @@ require(['main'], function(){
 });
 var attachSlider = function(){
   var div = document.createElement('div');
-  div.style.cssText = "position: absolute; width: 100px; top: -50px; color: white; border: 1px solid #333; padding: 0 7px;";
+  div.style.cssText = "position: absolute; width: 100px; top: -40px; color: white; border: 1px solid #333; padding: 0 7px; background: black;";
   div.innerHTML = '<div class="progress"><div class="seekbar"></div><div class="episode-progress" style="width: 61%;"></div></div>';
   document.querySelector('li.control.mute').appendChild(div);
 };
